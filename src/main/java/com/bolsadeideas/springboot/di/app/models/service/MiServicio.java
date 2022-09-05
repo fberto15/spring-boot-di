a5575e00e7d41a0297018b1b6cf4c60319a6b101
fberto15 <fberto15@gmail.com>
2022-09-05 20:31:48 -0300

    Initial commit

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("miServicioSimple")
+public class MiServicio implements IServicio {
+	
+	@Override
+	public String operacion() {
+		return "ejecutando...";		
+	}
+
+}
